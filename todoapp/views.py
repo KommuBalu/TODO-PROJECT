fb3197c5908923320e77c42e2ebc8a5c724d24f6
Kommu Pawan <kommupawan84@gmail.com>
2026-08-04 00:20:04 +0530

    Add Django Todo project

---
+++ todoapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import redirect, render,HttpResponse
+from django.template import context
+from .forms import RegisterForm
+
+# Create your views here.
+def home(request):
+    return render(request,"index.html")
+
+
+def register_view(request):
+
+    if request.method == "POST":
+        form= RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request,"register.html",{'form': form})
+
+
+
+
+def login_view(request):
+    return render(request,"login.html")
+
+def logout_view(request):
+    return HttpResponse("Logout")
+
+def delete_task(request,id):
+    return HttpResponse("delete") 
