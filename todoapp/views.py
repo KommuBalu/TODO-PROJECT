c7c4be04a568a011f29c5da63a6a0cbb305594dc
Kommu Pawan <kommupawan84@gmail.com>
2026-08-04 13:00:37 +0530

    Added Django admin and task management

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,8 +1,14 @@
+from django.http import request
 from django.shortcuts import redirect, render,HttpResponse
 from django.template import context
 from .forms import RegisterForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
+@login_required
 def home(request):
     return render(request,"index.html")
 
@@ -22,10 +28,24 @@ def register_view(request):
 
 
 def login_view(request):
-    return render(request,"login.html")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("home")   # lowercase — must match urls.py name
+        else:
+            return render(request, "login.html", {"error": "Invalid username or password"})
+    return render(request, "login.html")
+
+
+
+@login_required
 def logout_view(request):
-    return HttpResponse("Logout")
+    logout(request)
+    return redirect("login")
 
 def delete_task(request,id):
     return HttpResponse("delete") 
